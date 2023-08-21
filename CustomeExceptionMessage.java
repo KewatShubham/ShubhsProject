bb1926b832c0b7cc2bdc88392722dc05310f6804
Shubham Kewat <45590003+KewatShubham@users.noreply.github.com>
2023-08-21 12:13:06 +0530

    Add files via upload

---
+++ CustomeExceptionMessage.java
@@ -0,0 +1,11 @@
+public class CustomeExceptionMessage extends Throwable {
+	private String exceptionMsg;
+
+	public CustomeExceptionMessage(String exceptionMsg) {
+		this.exceptionMsg = exceptionMsg;
+	}
+
+	public String getExceptionMessage() {
+		return this.exceptionMsg;
+	}
+}
